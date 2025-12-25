20a28468122d6323fe0f070988bf6170b911e0e8
penofcilin <63890812+penofcilin@users.noreply.github.com>
2025-12-24 22:56:08 -0500

    update

--- Source/ui/react/src/sections/HeaderSection.jsx
+++ Source/ui/react/src/sections/HeaderSection.jsx
@@ -20,6 +20,7 @@ export default function HeaderSection() {
 
           color: "white",
           fontFamily: "var(--font-sans)",
+          fontWeight: "bold",
         }}
       >
         Tremokitty!
